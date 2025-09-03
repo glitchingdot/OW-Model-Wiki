b3a7610aa730a220e00b33a5cd0096f9096ebd4b
glitchingdot <jack@glitching.xyz>
2025-09-02 20:44:26 -0700

    Added Search

--- components/TransparentVideo.vue
+++ components/TransparentVideo.vue
@@ -4,8 +4,8 @@ defineProps(['path'])
 
 <template>
 <video autoplay muted loop playsinline>
-  <source :src="`./media/${path}/output-1.mov`" type='video/mp4; codecs=hvc1'>
-  <source :src="`./media/${path}/output.webm`" type="video/webm">
+  <source :src="`/media/${path}/output-1.mov`" type='video/mp4; codecs=hvc1'>
+  <source :src="`/media/${path}/output.webm`" type="video/webm">
 </video>
 
 <a :href="`./media/${path}/output.webm`">Open in Full Size</a>
